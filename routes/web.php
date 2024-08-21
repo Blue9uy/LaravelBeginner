86082c5661df0837e64cfd704cc5c08b63ea1868
KWH <macbookpro2017@MacBooks-MacBook-Pro.local>
2024-08-21 17:55:07 +0630

    Routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route ::get("/admin/posts", function () {
+    return "Hello Admin Posts!!!!!";});
+
+    Route ::get("/blog", function () {
+        return "Hello blog page";});
+
+Route ::get("/post/{id}/{name}", function ($id,$name) {
+    return "Your Post is ". $id ." ". $name;});
+
+Route ::get("/admin/dashboard/post/edit/",['as'=>'admin.post', function () {
+    $url = route('admin.post');
+    return "your url is".$url;}]);
