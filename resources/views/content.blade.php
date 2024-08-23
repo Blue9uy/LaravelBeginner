5ee749e86844d0734b8651daa27d77af84050a9d
KWH <macbookpro2017@MacBooks-MacBook-Pro.local>
2024-08-23 20:03:23 +0630

    Master Blade Template

---
+++ resources/views/content.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Content Page</h1>
+    <div>
+        @if (count($students))
+            <p>Students Data</p>
+            <ol>
+                @foreach ($students as $student)
+                    <li>{{$student}}</li>
+                @endforeach
+            </ol>
+        @endif
+    </div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1>Home Page is Changing Here</h1>
+@endsection
