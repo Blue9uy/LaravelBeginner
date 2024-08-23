4ac1c1a2a14ee4a1cb6206bd9b55ea2b0ed4c962
KWH <macbookpro2017@MacBooks-MacBook-Pro.local>
2024-08-23 15:01:33 +0630

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\Controller;
+Route::get('/post',PostController::class .'@index');
