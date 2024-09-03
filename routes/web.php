0b1e011be348398904ca1ebcba58d57cee09b2b6
KWH <macbookpro2017@MacBooks-MacBook-Pro.local>
2024-09-03 21:12:29 +0630

    RAW SQL Queries

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/insert", function () {
+    DB::insert(
+    "insert into posts(title,body) value(?,?)",
+    ['PHP laravel','Laravel is a best PHP framework']);
+    return 'insert successful';
+});
+Route::get('/show', function () {
+    $posts=DB::select('select * from posts');
+    return $posts;
+});
+Route::get('/update', function () {
+    $result=DB::update('update posts set title = ? where id = ?', ['Update title',1]);
+    return $result;
+});
+Route::get('/delete', function () {
+    $result=DB::delete('delete from posts where id = ?', [1]);
+    return $result;
+
 });
